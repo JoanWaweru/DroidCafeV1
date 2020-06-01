d0178e66e92a450128042e7aede5059ded86ba32
Righa <54580993+Righa@users.noreply.github.com>
2020-06-01 17:18:51 +0300

    recycler view part 3

--- app/src/main/java/com/example/droidcafev1/DessertRecipeFragment.java
+++ app/src/main/java/com/example/droidcafev1/DessertRecipeFragment.java
@@ -1,18 +1,26 @@
 package com.example.droidcafev1;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DessertRecipeFragment extends Fragment {
+    private RecyclerView dessertsRecyclerView;
+    private ArrayList<Recipe> dessertRecipeData;
+    private RecipeAdapter dessertAdapter;
 
     public DessertRecipeFragment() {
         // Required empty public constructor
@@ -23,6 +31,29 @@ public class DessertRecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dessert_recipe, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dessert_recipe, container, false);
+
+        dessertsRecyclerView = rootView.findViewById(R.id.recycler_desert);
+        dessertsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        dessertRecipeData = new ArrayList<>();
+        dessertAdapter = new RecipeAdapter(dessertRecipeData, getContext());
+        dessertsRecyclerView.setAdapter(dessertAdapter);
+        initializeData();
+        return rootView;
+    }
+
+    private void initializeData() {
+        String[] dessertTitles = getResources().getStringArray(R.array.desserts_titles);
+        String[] dessertDescriptions = getResources().getStringArray(R.array.desserts_descritions);
+        TypedArray dessertImages = getResources().obtainTypedArray(R.array.desserts_images);
+
+        dessertRecipeData.clear();
+
+        for (int i = 0; i < dessertTitles.length; i++) {
+            dessertRecipeData.add(new Recipe(dessertImages.getResourceId(i,0), dessertTitles[i], dessertDescriptions[i]));
+        }
+
+        dessertImages.recycle();
+        dessertAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/example/droidcafev1/RecipeAdapter.java
@@ -0,0 +1,66 @@
+package com.example.droidcafev1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    private ArrayList<Recipe> recipeData;
+    private Context myContext;
+
+    public RecipeAdapter(ArrayList<Recipe> recipeData, Context myContext) {
+        this.recipeData = recipeData;
+        this.myContext = myContext;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(myContext).inflate(R.layout.recipe_list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Recipe currentRecipe = recipeData.get(position);
+        
+        holder.bindTo(currentRecipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView myRecipeImage;
+        private TextView myRecipeTitle;
+        private TextView myRecipeDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            myRecipeImage = itemView.findViewById(R.id.recipe_image);
+            myRecipeTitle = itemView.findViewById(R.id.recipe_title);
+            myRecipeDescription = itemView.findViewById(R.id.recipe_description);
+        }
+
+        public void bindTo(Recipe currentRecipe) {
+            Glide.with(myContext).load(currentRecipe.getRecipeImage()).into(myRecipeImage);
+            myRecipeTitle.setText(currentRecipe.getRecipeTitle());
+            myRecipeDescription.setText(currentRecipe.getRecipeDescription());
+
+        }
+    }
+}
